770aa02e2e946390001ba24f9c5301543acadfe1
Batemir Karuev <31863592+batemir@users.noreply.github.com>
2019-01-13 21:29:08 +0300

    Add files via upload

--- formHandle.php
+++ formHandle.php
@@ -1,36 +1,34 @@
 <?php 
 
-//загружаю на сервер 
-
 $tmp = $_FILES['userfile']['tmp_name']; 
 $filename = $_FILES['userfile']['name']; 
 move_uploaded_file($tmp, './uploads/'.$filename); 
 
 
-$filepath = realpath('./uploads/'.$filename); 
-$file = fopen($filepath, 'w+'); 
-$post = array('file_contents' => $file); 
+$filepath = realpath('./uploads/'.$filename);
+$post = array('file_content' => '@'.$filepath); 
 
 
-$fp = fopen('./error.log', 'w+'); 
 $ch = curl_init(); 
 curl_setopt_array($ch, array( 
-CURLOPT_URL => 'https://api.voximplant.com/platform_api/CreateCallList/?accou..', 
+CURLOPT_URL => 'http://api.voximplant.com/platform_api/CreateCallList/?account_id=2482237&api_key=ed0d0ea5-aeb3-4dd0-95a3-025c781bc957&rule_id=2299546&priority=1&max_simultaneous=2&num_attempts=2&name=callList', 
 CURLOPT_RETURNTRANSFER => true, 
 CURLOPT_POST => true, 
-CURLOPT_POSTFIELDS => $post, 
-CURLOPT_VERBOSE => true, 
-CURLOPT_STDERR => $fp, 
+CURLOPT_POSTFIELDS => $post,  
 CURLOPT_SSL_VERIFYPEER => false, 
 CURLOPT_HTTPHEADER => array( 
 'Content-type' => 'text/csv' 
 ) 
-)); 
-curl_setopt($ch, CURLOPT_HEADER, 1); 
+));  
 
 
 $res = curl_exec($ch); 
-curl_close($ch); 
-fclose($fp); 
-fclose($file); 
-var_dump($res);
+curl_close($ch);
+
+$result = json_decode($res, true);
+if ($result['result']) {
+    echo 'Список загружен и запущен!';
+} else {
+    echo 'Ошибка';
+    echo ($result['error']['msg']);
+}
